32db39d3d81c80e0efb8201d540c5872c580a4ed
HannaBayerlein <dic14hba@student.lu.se>
2018-09-24 09:51:39 +0200

    DH, HMAC och Encrypt

--- client.py
+++ client.py
@@ -1,46 +1,64 @@
 import socket
 import sys
 import select
+import ssl
+import random
+from Crypto import Random
+from Crypto.Cipher import AES
+import hmac
+import hashlib
+import base64
+from Crypto.Hash import HMAC
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server_address = ('localhost', 10000)
-message = 'This is the message.  It will be repeated.'
+
+sharedPrime = 12973
+sharedBase = 14479
+clientSec = random.randint(1000,1100)
+print("client sec", clientSec)
+clientSecret = str(sharedBase ** clientSec % sharedPrime)
+print("C", clientSecret)
+sock.sendto(clientSecret.encode('utf-8'), server_address)
+serverSecret = sock.recv(2048)
+serverSecret.decode('utf-8')
+if serverSecret:
+    print("S", serverSecret)
+    commonSecret = int(serverSecret) ** int(clientSecret) % sharedPrime
+    print("Common", commonSecret)
+
+#iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
 
 
 while True:
-
+    iv = Random.new().read(AES.block_size)
+    encodeSecret = str(commonSecret)
+    bitKey = encodeSecret.encode('utf-8')
+    key = encodeSecret.ljust(16)
+    obj = AES.new(key, AES.MODE_CBC, iv)
     # maintains a list of possible input streams
     sockets_list = [sys.stdin, sock]
-
     read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
 
     for socks in read_sockets:
         if socks == sock:
             message = socks.recv(2048)
-            print message
+            print("rec",message)
+            #decryptMessage = str(message)
+            obj2 = AES.new(key, AES.MODE_CBC, ivTemp)
+            print("Riktigt mes", obj2.decrypt(message))
         else:
             message = sys.stdin.readline()
-            sock.sendto(message, server_address)
+            encryptMessage = obj.encrypt(message.ljust(16))
+            #sign = base64.b64encode(hmac.new(bitKey, encryptMessage, digestmod=hashlib.sha256).digest())
+            #print("Sign",sign)
+            sign = HMAC.new(encryptMessage).digest()
+            print("Sign", sign)
+        #    print("digest", sign.hexdigest())
+            sock.sendto(sign, server_address)
+            ivTemp = iv
             #sys.stdout.write(message)
-            sys.stdout.flush()
+            #sys.stdout.flush()
 sock.close()
-
-
-
-
-#try:
-
-    # Send data
-#    print >>sys.stderr, 'sending "%s"' % message
-#    sent = sock.sendto(message, server_address)
-
-    # Receive response
-#    print >>sys.stderr, 'waiting to receive'
-#    data, server = sock.recvfrom(4096)
-#    print >>sys.stderr, 'received "%s"' % data
-
-#finally:
-#    print >>sys.stderr, 'closing socket'
-#    sock.close()
--- server.py
+++ server.py
@@ -1,56 +1,41 @@
 import socket
 import sys
+import random
+from Crypto.Cipher import AES
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
 server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
+sharedPrime = 12973
+sharedBase = 14479
+serverSec = random.randint(1000,1100)
+print("server", serverSec)
+serverSecret = str((sharedBase ** serverSec) % sharedPrime)
+print("S", serverSecret)
+clientSecret, address = sock.recvfrom(4096)
+clientSecret.decode('utf-8')
+int(clientSecret)
+print("secC", clientSecret)
+if clientSecret:
+    sent = sock.sendto(serverSecret.encode('utf-8'), address)
+
+commonSecret = int(serverSecret) ** int(clientSecret) % sharedPrime
+print("Common", commonSecret)
+
+
 while True:
-    print >>sys.stderr, '\nwaiting to receive message'
+    print('\nwaiting to receive message')
     data, address = sock.recvfrom(4096)
 
-    print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
-    print >>sys.stderr, data
+    print('received %s bytes from %s' % (len(data), address))
+    #sent = sock.sendto(data, address)
+    #print(data)
 
     if data:
         sent = sock.sendto(data, address)
-        print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
-
-
-
-# use Python 3 print function
-# this allows this code to run on python 2.x and 3.x
-#from __future__ import print_function
-
-# Variables Used
-#sharedPrime = 23    # p
-#sharedBase = 5      # g
-
-#aliceSecret = 6     # a
-#bobSecret = 15      # b
-
-# Begin
-#print( "    Publicly Shared Prime: " , sharedPrime )
-#print( "    Publicly Shared Base:  " , sharedBase )
-
-# Alice Sends Bob A = g^a mod p
-#A = (sharedBase**aliceSecret) % sharedPrime
-#print( "\n  Alice Sends Over Public Chanel: " , A )
-
-# Bob Sends Alice B = g^b mod p
-#B = (sharedBase ** bobSecret) % sharedPrime
-#print( &amp;amp;amp;amp;amp;amp;quot;   Bob Sends Over Public Chanel: ", B )
-
-#print( "\n------------\n" )
-#print( "Privately Calculated Shared Secret:" )
-# Alice Computes Shared Secret: s = B^a mod p
-#aliceSharedSecret = (B ** aliceSecret) % sharedPrime
-#print( "    Alice Shared Secret: ", aliceSharedSecret )
-
-# Bob Computes Shared Secret: s = A^b mod p
-#bobSharedSecret = (A**bobSecret) % sharedPrime
-#print( "    Bob Shared Secret: ", bobSharedSecret )
+        print(data)
+        #print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
